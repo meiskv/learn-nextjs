4fa7b52074314441d105a42dee816c35eff2ea0e
SXOF <SXOF@Mikhails-MacBook-Pro.local>
2024-01-19 10:16:11 +1300

    finished the nextjs learn tutorial

---
+++ app/lib/schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
+
+export const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export const UpdateInvoice = FormSchema.omit({ id: true, date: true });
